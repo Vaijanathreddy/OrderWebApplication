5f319109e8b168ce0973863a1f39f4a0efd328bb
vaijanathreddy <vaijanathreddygoure123@gmail.com>
2025-02-18 11:39:17 +0530

    Added order Model

---
+++ OrderApplication/Models/OrderProcess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrderApplication.Models
+{
+    public class OrderProcess
+    {
+        [Range(0.01, double.MaxValue, ErrorMessage = "Order amount must be greater than zero.")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string CustomerType { get; set; }
+
+        public decimal Discount { get; set; }
+        public decimal FinalTotal { get; set; }
+    }
+}
